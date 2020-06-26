5ca581150d715c6797a542f89a27e6f2b04b4c61
Nguyen Trung Tin <tin@tintrungnguyen.com>
2020-06-26 11:37:30 +0700

    Add error check for context

--- lib/error.go
+++ lib/error.go
@@ -7,19 +7,37 @@
 package lib
 
 import (
+	"context"
+
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // HandleErr check error, if err not nil, log to stdout
-func HandleErr(err error) {
+func LogErr(err error) {
 	if err != nil {
 		log.Error(err)
 	}
 }
 
-// MustErr check error, if err not nil, log and fatal
-func MustErr(err error) {
+// Must check error, if err not nil, log and fatal
+func Must(err error) {
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+// ContextErr check context error and return status and code base on current error
+func ContextErr(ctx context.Context) error {
+	LogErr(ctx.Err())
+
+	switch ctx.Err() {
+	case context.Canceled:
+		return status.Error(codes.Canceled, "request is canceled")
+	case context.DeadlineExceeded:
+		return status.Error(codes.DeadlineExceeded, "deadline is exceeded")
+	default:
+		return nil
 	}
 }
